2e12c025252805bf98c4ed77a19512a92a257ad2
Pims-creator <pierre.mattafiri@gmail.com>
2022-01-07 23:08:05 +0100

    first commit

---
+++ src/components/Comment.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div class="counter">
+      counter
+    </div>
+    <div class="content">
+      <div class="header">
+        <img src="" alt="avatar">
+        <span>name</span> 
+        <span>date</span>
+        <button>reply</button>
+      </div>
+      <div class="commentText">
+        commentaire 
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Comment',
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
